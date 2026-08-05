9818b07d49ea260afeae772ec28dabe3fd8973f0
Steven18AS <joelyandun2021@gmail.com>
2026-08-05 00:15:37 -0500

    Fix Flyway autoconfig, nginx DNS resolution, pgAdmin email

--- teacolito/build.gradle.kts
+++ teacolito/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-flyway")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	runtimeOnly("org.postgresql:postgresql")
